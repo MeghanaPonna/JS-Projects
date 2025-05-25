dd0a056abd4f4ee379a2684a8995633153b59d1b
Meghana <meghanaponna234@gmail.com>
2025-05-25 22:19:21 +0530

    Initial comment

---
+++ index.js
@@ -0,0 +1,74 @@
+// // console.log(`Hello`);
+// // console.log(`I Like Pizza`);
+
+
+// // create an alert
+// // window.alert(`This is an alert`);
+// // window.alert(`I like Pizza`);
+
+
+// //  to change the content of html element - first select elemt - then change the textcontent=''
+// document.getElementById("myH1").textContent='Hello';
+// document.getElementById("myP").textContent='I like pizza!';
+
+
+// // this is a comment
+// /*
+// this 
+// is 
+// a
+// comment
+// */
+
+
+// variables
+// let x;
+// x=100;
+// let y=100;
+// console.log(x);
+
+// datatype1 - number
+// let age = 20;
+// let price = 10.99;
+// let gpa = 2.1;
+// console.log(age);
+// console.log(price);
+// console.log(gpa);
+// console.log(`You are ${age} years old`);
+// console.log(`The price is: $${price}`);
+// console.log(`your gpa is: ${gpa}`);
+// console.log(typeof age);
+// console.log(typeof price);
+// console.log(typeof gpa);
+
+// datatype2 - strings --> a series of characters
+// let name1 = "megg";
+// let food = "biryani";
+// let email = "meg@gmail.com";
+// console.log(typeof name1);
+// console.log(`you name is : ${name1}`);
+// console.log(`your fav food is : ${food}`);
+// console.log(`your email: ${email}`);
+
+// booloean - true/false
+// let online = true;
+// console.log(typeof online);
+// console.log(`bro is online: ${online}`);
+
+
+// let fullName = "Bro Code";
+// let age = 25;
+// let isStudent = false;
+
+// document.getElementById("p1").textContent=fullName;
+// document.getElementById("p2").textContent=age;
+// document.getElementById("p3").textContent=isStudent;
+
+// document.getElementById("p1").textContent=`your fullname is: ${fullName}`;
+// document.getElementById("p2").textContent=`your age ${age} years old`;
+// document.getElementById("p3").textContent=`Enrolled: ${isStudent}`;
+
+
+
+
+
